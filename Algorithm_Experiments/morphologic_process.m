4925a08bc330a38b7b06e2f335855b9ddc9848af
Zheng Jingjie <i@tsenmu.com>
2012-07-01 15:25:06 +0800

    ++
    Signed-off-by: Zheng Jingjie <i@tsenmu.com>

--- Algorithm_Experiments/morphologic_process.m
+++ Algorithm_Experiments/morphologic_process.m
@@ -1,5 +1,5 @@
-function C = morphologic_process(I, diskSize)
-    E = edge(I, 'roberts');
-    C = imclose(E, strel('disk', diskSize));
-    C = imopen(C, strel('disk', diskSize));
+function C = morphologic_process(I)
+I = histeq(I);
+figure; imshow(I);
+C = edge(I, 'canny');
 end
