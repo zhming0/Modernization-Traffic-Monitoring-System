8b9a1ab405a9c917c57eacf31f0aa2ee94f9a368
Zheng Jingjie <i@tsenmu.com>
2012-07-01 12:50:38 +0800

    find_plate_area modified.
    Signed-off-by: Zheng Jingjie <i@tsenmu.com>

--- Algorithm_Experiments/find_plate_area.m
+++ Algorithm_Experiments/find_plate_area.m
@@ -7,20 +7,37 @@ function R = find_plate_area(src, widerplate, h, Cd)
     I = src(y1:y2, x1:x2);
     %figure; imshow(I);
     V = horizontal_intensity_projection(I);
-    %V
+%     V = rank_filter(double(V), 9);
     %figure; plot(V);
     Vdiff = diff_h(V, h);
     %Vdiff
     %size(Vdiff)
-    figure; plot(Vdiff);
+%     figure; plot(Vdiff);
     Vdiff_len = length(Vdiff);
     Vdiff_len_half_toInt = int32(Vdiff_len/2);
     Vdiff_min = min(Vdiff) * Cd;
     Vdiff_max = max(Vdiff) * Cd;
     Vdiff_left = Vdiff(1:Vdiff_len_half_toInt);
     Vdiff_right = Vdiff(Vdiff_len_half_toInt:end);
-    [~,left] = max((Vdiff_left <= Vdiff_min) .* Vdiff_left);
-    [~,right] = min((Vdiff_right >= Vdiff_max) .* Vdiff_right);
+    
+     MAX = -100000000; MIN = 100000000;
+     left = 1; right = 1;
+    for i = 1 : length(Vdiff_left)
+        if (Vdiff_left(i) <= Vdiff_min && Vdiff_left(i) >=  MAX)
+            MAX = Vdiff_left(i);  
+            left = i;
+        end
+    end
+    for i = 1 : length(Vdiff_right)
+        if (Vdiff_right(i) >= Vdiff_max && Vdiff_right(i) <=  MIN)
+            MIN = Vdiff_right(i);  
+            right = i;
+        end
+    end
+    
+%     [~,right] = max(((Vdiff_right <= Vdiff_min)~=0) .* Vdiff_right);
+%     plot(((Vdiff_right <= Vdiff_min)~=0) .* Vdiff_right);
+%     [~,left] = min(((Vdiff_left >= Vdiff_max)~=0) .* Vdiff_left);
     %R = I(:, left:right);
     %figure; plot(Vdiff_right);
     R = [x1 + left, x1 + right + Vdiff_len_half_toInt, y1, y2];
